ed703f83de22c0c3835f465b3bf708c79e4bc7f3
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-02-16 17:00:41 +1030

    updated program with variables replacing hardcoded values for length and width of chessboard

--- chessboard.js
+++ chessboard.js
@@ -1,11 +1,13 @@
 let chessboard = "#";
 let lines = 0;
+let width = 8;
+let length = 8;
 
 for (let i = 1; ; i++) {
-	if (lines == 8) {
+	if (lines == length) {
 		break;
 	}
-	else if (chessboard.length == 8) {
+	else if (chessboard.length == width) {
 	console.log(chessboard);
 	chessboard = "";
 	i -= 2;
